5ae7b600342363cf520450aad7cc9c0816fe6165
Rahul singh <rahulsinghmp968@gmail.com>
2023-02-18 22:37:37 +0530

    added JWt mecha

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -3,5 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/resturant", require("./resturant"));
+router.use("/resturantOwner", require("./resturantOwner"));
 
 module.exports = router;
